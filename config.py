26568b398a0ff25739ddc7b25c23bae2b68d9dbf
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-08-11 16:32:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 api_id = int(os.environ.get("API_ID", "22108820"))
 api_hash = os.environ.get("API_HASH", "301a39af78de73406a67c75ad7dd7301")
-bot_token = os.environ.get("BOT_TOKEN", "6196461165:AAFZ0fX5-F2gMb0JNQauTJma2ljHRwyiEeU")
+bot_token = os.environ.get("BOT_TOKEN", "6692168439:AAGg0afMKxJh2y_rsV6BHVDPwc64qgxDZ1c")
 # =========================================================== #
 
 db_url = os.environ.get("DB_URL", "mongodb+srv://fadhil01:fadhil123@fadhil01.s6lkqsq.mongodb.net/?retryWrites=true&w=majority")
@@ -10,11 +10,11 @@ db_name = os.environ.get("DB_NAME", "fadhil01")
 # =========================================================== #
 
 channel_1 = int(os.environ.get("CHANNEL_1", "-1001509543098"))
-channel_2 = int(os.environ.get("CHANNEL_2", "-1001956779347"))
-channel_log = int(os.environ.get("CHANNEL_LOG", "-1001964399865"))
+channel_2 = int(os.environ.get("CHANNEL_2", "-1001961436814"))
+channel_log = int(os.environ.get("CHANNEL_LOG", "-1001915672497"))
 # =========================================================== #
 
-id_admin = int(os.environ.get("ID_ADMIN", "6103178771"))
+id_admin = int(os.environ.get("ID_ADMIN", ""))
 # =========================================================== #
 
 batas_kirim = int(os.environ.get("BATAS_KIRIM", "3"))
@@ -38,15 +38,15 @@ biaya_bfrent = int(os.environ.get("BIAYA_BFRENT", "500"))
 hastag = os.environ.get("HASTAG", "#fwbcantik #fwbganteng #fwbgalau #fwbrandom #fwbpap #fwbkepo").replace(" ", "|").lower()
 # =========================================================== #
 
-pic_boy = os.environ.get("PIC_BOY", "https://telegra.ph//file/ab262a6eed7b5e4879ae3.jpg")
-pic_girl = os.environ.get("PIC_GIRL", "https://telegra.ph//file/f189097501089f5cc38dd.jpg")
-pic_talent = os.environ.get("PIC_TALENT", "https://telegra.ph//file/30c3b445e0c33c90cb0b6.jpg")
-pic_bf_rent = os.environ.get("PIC_BF_RENT", "0")
-pic_gf_rent = os.environ.get("PIC_GF_RENT", "0")
+pic_boy = os.environ.get("PIC_BOY", "https://telegra.ph//file/67693ef3e1a1bf41c4632.jpg")
+pic_girl = os.environ.get("PIC_GIRL", "https://telegra.ph//file/4d228a9ac5ea990d9c381.jpg")
+pic_talent = os.environ.get("PIC_TALENT", "https://telegra.ph//file/3779f7368bed17557c71a.jpg")
+pic_bf_rent = os.environ.get("PIC_BF_RENT", "https://telegra.ph//file/44e90c837fdded8a9bb8d.jpg")
+pic_gf_rent = os.environ.get("PIC_GF_RENT", "https://telegra.ph//file/225355e06d730073285b2.jpg")
 # =========================================================== #
 
 pesan_join = os.environ.get("PESAN_JOIN", "Hai {mention} Sobat FWB😉\n\nKamu Tidak dapat Mengirim Menfes , Harap Join Terllebih Dahulu Untuk Mengirim Menfess ya FWB👍")
-start_msg = os.environ.get("START_MSG", "Hai {fullname} hallo selamat datang dibot auto post FWB base✨\n\nIni adalah bot Menfes ya FWB, semua pesan yang kamu kirim akan masuk ke channel secara anonim sesuai hastag:\n#fwbrandom Untuk Random\n#fwbkepo untuk Bertanya\n#fwbgalau untuk Berbagi Cerita\n#fwbpap untuk ngirim pap kecuali pap kemaluan\n\nContoh:\n {mention} Cari Mutualan Dom Depok #fwbganteng/n Contact @chloppii")
+start_msg = os.environ.get("START_MSG", "Hai {fullname} hallo selamat datang dibot auto post FWB base✨\n\nIni adalah bot Menfes ya FWB, semua pesan yang kamu kirim akan masuk ke channel secara anonim sesuai hastag:\n#fwbrandom Untuk Random\n#fwbkepo untuk Bertanya\n#fwbgalau untuk Berbagi Cerita\n#fwbpap untuk ngirim pap kecuali pap kemaluan\n\nContoh:\n {mention} Cari Mutualan Dom Depok #fwbganteng/n Contact @Stevvano")
 
 gagalkirim_msg = os.environ.get("GAGAL_KIRIM", """
 {mention}, pesan mu gagal terkirim silahkan gunakan hastag:
